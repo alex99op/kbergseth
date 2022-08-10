3bb4712a40e8023d2603cdf9857ec3075429c8f8
alex99op <alexander.ronasen@gmail.com>
2022-08-10 13:09:15 +0200

    Bedret_kode

--- bolla.py
+++ bolla.py
@@ -1,24 +1,26 @@
-#åpner fil og lager liste
-with open("bolla.txt", "r") as primtall_liste:
-	lines = primtall_liste.readlines()
-#print(lines)
-
-#Alle tallene ender med /n, dermed er det en enkel måte å skille på dem. Evt må det også fjernes før man kan
-#begynne å regne. Split opp evt ved en loop, eller så burde det vell finnes en smart funksjon å gjøre det på.
-#FIXA DET
-
 '''Psaudokode: skal ta ut hver linje fra filen bolla.txt
 og legge til en liste, gå gjennom listen og plukke ut
 primtallene og legge til en ny liste, deretter bruke 
 long_to_bytes() for å finne flagget.
 '''
+from sympy.ntheory import isprime
+primtall=[]
+
+#åpner fil og lager liste
+with open("bolla.txt", "r") as primtall_liste:
+	lines = primtall_liste.readlines()
+
+
 def is_prime(liste):
 	for i in liste:
 		i.replace('\n', '')
-		print(int(i) /2) #HER skal du legge inn hva man skal dele på for å finne ut om det er et primtall
+		 #HER skal du legge inn hva man skal dele på for å finne ut om det er et primtall
+		if (isprime(i)) == True:
+			primtall.append(i)
 
 
 is_prime(lines)
+print(primtall)
 '''
 def is_prime(n):
 	for i in range(2,n):
